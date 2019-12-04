98ec6fb83652db0e6c65d5bb82437ae6975faa18
Luca <53027356+ldicarlo1@users.noreply.github.com>
2019-12-04 17:59:48 +0000

    Update help.py

--- help.py
+++ help.py
@@ -29,103 +29,69 @@ print(T2m)
 https://nomads.ncep.noaa.gov:9090/dods/gfs_0p50/gfs20191204/gfs_0p50_06z.info
 
 
+def connect_00z():
+    now = datetime.today().strftime('%Y%m%d')
+    file = Dataset('https://nomads.ncep.noaa.gov:9090/dods/gfs_0p50/gfs'+now+'/gfs_0p50_00z')
+
+    return file 
+    
+def connect_06z():
+    now = datetime.today().strftime('%Y%m%d')
+    file = Dataset('https://nomads.ncep.noaa.gov:9090/dods/gfs_0p50/gfs'+now+'/gfs_0p50_06z')
+
+    return file 
 
 def query_server(model_run):
-    '''Queries the THREDDS server for variables of interest.
-    Converts variables into correct units.
-    Fills in 00z data..
-    Returns all variables.
-    '''
-    #start a query
     if model_run == '06z':
-        ncss = connect_06z()
+        ds = connect_06z()
         print('GFS 06z Half Degree Model Chosen')
     elif model_run == '00z':
-        ncss = connect_00z()
+        ds = connect_00z()
         print('GFS 00z Half Degree Model Chosen')
     else:
         print('Enter either 0z or 6z')
-   #start a query
-    #all variables will be a length of 129, carrying timestamps from 00z init date to 00z end date
-    query = ncss.query()
-    query.accept('netcdf')
-    query.variables('Pressure_reduced_to_MSL_msl').all_times()
-    data = ncss.get_data(query)
-    time_mslp = data.variables[(list(data.variables))[1]][:].squeeze()
-    mslp = data.variables['Pressure_reduced_to_MSL_msl'][:].squeeze()
+
+    lat = ds.variables['lat'][:]
+    lon  = ds.variables['lon'][:]
+
+    T2m = ds.variables['tmp2m'][:]
+    #converts to Kelvin
+    T2m = T2m - 273.15
+
+    z500 = ds.variables['hgtprs'][:,20]
+    #convert to dm
+    z500 = z500/10
+
+    mslp = ds.variables['prmslmsl'][:]
+    #convert to mb
     mslp = mslp/100
-    mslp = data_adjustment(mslp,time_mslp)
 
-    query = ncss.query()
-    query.accept('netcdf')
-    query.variables('Downward_Short-Wave_Radiation_Flux_surface_Mixed_intervals_Average').all_times()
-    data = ncss.get_data(query)
-    SSRD = data.variables['Downward_Short-Wave_Radiation_Flux_surface_Mixed_intervals_Average'][:].squeeze()
-    time_SSRD = data.variables[(list(data.variables))[1]][:].squeeze()
-    SSRD = data_adjustment(SSRD,time_SSRD)
-    
-    
-    query = ncss.query()
-    query.accept('netcdf')
-    query.variables('Total_precipitation_surface_Mixed_intervals_Accumulation').all_times()
-    data = ncss.get_data(query)
-    TP = data.variables['Total_precipitation_surface_Mixed_intervals_Accumulation'][:].squeeze()  
-    #TP time steps go 3z,6z,9z,9z,12z,12z,15z,15z, etc
-    #we want accumulated only, which is every first time step, problem is that 3z and 6z aren't repeated
-    #we pull 6z out
-    TP_6z = TP[1]
-    #time steps now look like this 3z,9z,9z,12z,12z,15z,15z
-    TP = TP[0::2]
-    # time steps now look like this 3z,9z,12z,15z
-    # add 6z back in 
-    TP = np.insert(TP,1,TP_6z[0], axis=0)
-    #insert 0z to first spot
-    TP_0 = TP[0] - TP[0]
-    TP = np.insert(TP,0,TP_0,axis=0) 
-    # timesteps now are regular 0z,3z,6z,9z,12z,15z
+    SSRD = ds.variables['dswrfsfc'][:]
+    #replaces initial time step with 0's instead of NaNs
+    SSRD[0] = np.zeros((361,720))
 
-    #latitude is constant but longitude must be combined for east/west of prime meridian correction  
-    lat = data.variables['lat'][:].squeeze()
-    lon = data.variables['lon'][:].squeeze()
+    TP = ds.variables['apcpsfc'][:]
+    #replaces initial time step with 0's instead of NaNs
+    TP[0] = np.zeros((361,720))
 
-    query = ncss.query()
-    query.accept('netcdf')
-    query.variables('Temperature_height_above_ground').all_times().vertical_level(2)
-    data = ncss.get_data(query)
-    time_T2m = data.variables[(list(data.variables))[1]][:].squeeze()
-    T2m = data.variables['Temperature_height_above_ground'][:].squeeze() 
-    T2m = T2m - 273.15  
-    T2m = data_adjustment(T2m,time_T2m)
-
-    
-    #creates query
-    query = ncss.query()
-    query.accept('netcdf')
-    #specifies that we would like to collect the whole time extent of the GFS model 
-    query.variables('u-component_of_wind_height_above_ground','v-component_of_wind_height_above_ground').all_times().vertical_level(100)
-    #specifies we want wind at 100 meters
-    data = ncss.get_data(query)
-    time_w100 = data.variables[(list(data.variables))[1]][:].squeeze()
-    u_wind = data.variables['u-component_of_wind_height_above_ground'][:].squeeze()
-    v_wind = data.variables['v-component_of_wind_height_above_ground'][:].squeeze()
+    u_wind = ds.variables['ugrd100m'][:]
+    v_wind = ds.variables['vgrd100m'][:]
     #calculate the wind direction by U and V components
     w100 = np.sqrt((u_wind*u_wind) + (v_wind*v_wind))
-    w100 = np.array(w100)
-    w100 = data_adjustment(w100,time_w100)
-    
-    query = ncss.query()
-    query.accept('netcdf')
-    query.variables('Geopotential_height_isobaric').all_times().vertical_level(50000) 
-    data = ncss.get_data(query)
-    time_z500 = data.variables[(list(data.variables))[1]][:].squeeze()
-    z500 = data.variables['Geopotential_height_isobaric'][:].squeeze()
-    z500 = z500/10
-    z500 = data_adjustment(z500, time_z500)
 
-    del(query,data,ncss,u_wind,v_wind)
     
     return T2m,mslp,z500,w100,SSRD,TP,lat,lon 
-    
-    
-    #all must be in 129,361,720 format.
+
+
+T2m,mslp,z500,w100,SSRD,TP,lat,lon = query_server('06z')
+
+
+
+
+
+
+
+
+
+
     
