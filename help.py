36094c22cbd4c3a0a72f2440067fce87066981e7
Luca <53027356+ldicarlo1@users.noreply.github.com>
2020-01-12 11:17:05 +0000

    Update help.py

--- help.py
+++ help.py
@@ -1,89 +1,41 @@
-
-import numpy as np
+from netCDF4 import Dataset
+import numpy as np 
+import cartopy.crs as ccrs
 import matplotlib.pyplot as plt
-import netCDF4
+import pygrib
+from datetime import datetime, timedelta
 
+x = []
+bias = []
+num = 95               # June 13 is the first day that includes the GFS 0.5 degree update; specifies which index is the start of the 13th of June
+month = 6                 # the starting month of the loop being June
+for i in range(0,110):    # 110 days is the total length of days that we have reanalysis data for since June 13th   
+    date = (datetime(2019,6,13) + timedelta(days=i)).strftime('%Y%m%d')
+    month = date[4:6]
+    timestep = np.array(['00','03','06','09','12','15','18','21'])
+    bias.append(x)
+    for j in range(0, len(timestep)):      #loops through for 8 time steps within each day
+        file = '/mnt/naslsc2nwp/GFS/data/'+date+'/gfs.t00z.pgrb2.0p50.f0'+timestep[j]+''
+        grb = pygrib.open(file)
+        t2m = grb.select(name='Temperature')[0].values - 273.15
+        t2m = np.array(t2m)
+        t2m_west_of_meridian = np.array(t2m[30:121,660:720]) 
+        t2m_east_of_meridian = np.array(t2m[30:121,0:91])
+        t2m_oper = np.concatenate((t2m_west_of_meridian,t2m_east_of_meridian),axis=1) #adjusts longitude to be from -30 to 45
+        
+        file2 = '/home/luca/Desktop/Bias Project/all_vars_2019_'+month+'.nc'
+        nc = Dataset(file2)
+        t2m_era5 = nc.variables['t2m'][:]
+        t2m_era5 = np.array(t2m_era5)
+        t2m_era5 = np.where(t2m_era5==-32767,np.nan,t2m_era5)
+        diff = t2m_oper - t2m_era5[num]    #sets ocean data to NaN
+        num+=1
+        if num == len(t2m_era5):
+            num = 0 
+        x.append(diff)
+bias = np.array(bias)
+#bias_average = np.nanmean()
 
-
-# set up the URL to access the data server.
-# See the NWW3 directory on NOMADS 
-# for the list of available model run dates.
-
-mydate='20191204'
-url='https://nomads.ncep.noaa.gov:9090/dods/gfs_0p50/gfs20191204/gfs_0p50_06z'
-
-# Extract the significant wave height of combined wind waves and swell
-
-file = netCDF4.Dataset(url)
-lat  = file.variables['lat'][:]
-lon  = file.variables['lon'][:]
-T2m = file.variables['tmp2m'][:].squeeze() - 273.15
-z500 = file.variables['hgtprs'][:,20]
-
-level = file.variables['lev']
-
-print(T2m)
-
-
-
-https://nomads.ncep.noaa.gov:9090/dods/gfs_0p50/gfs20191204/gfs_0p50_06z.info
-
-
-def connect_00z():
-    now = datetime.today().strftime('%Y%m%d')
-    file = Dataset('https://nomads.ncep.noaa.gov:9090/dods/gfs_0p50/gfs'+now+'/gfs_0p50_00z')
-
-    return file 
-    
-def connect_06z():
-    now = datetime.today().strftime('%Y%m%d')
-    file = Dataset('https://nomads.ncep.noaa.gov:9090/dods/gfs_0p50/gfs'+now+'/gfs_0p50_06z')
-
-    return file 
-
-def query_server(model_run):
-    if model_run == '06z':
-        ds = connect_06z()
-        print('GFS 06z Half Degree Model Chosen')
-    elif model_run == '00z':
-        ds = connect_00z()
-        print('GFS 00z Half Degree Model Chosen')
-    else:
-        print('Enter either 0z or 6z')
-
-    lat = ds.variables['lat'][:]
-    lon  = ds.variables['lon'][:]
-
-    T2m = ds.variables['tmp2m'][:]
-    #converts to Kelvin
-    T2m = T2m - 273.15
-
-    z500 = ds.variables['hgtprs'][:,20]
-    #convert to dm
-    z500 = z500/10
-
-    mslp = ds.variables['prmslmsl'][:]
-    #convert to mb
-    mslp = mslp/100
-
-    SSRD = ds.variables['dswrfsfc'][:]
-    #replaces initial time step with 0's instead of NaNs
-    SSRD[0] = np.zeros((361,720))
-
-    TP = ds.variables['apcpsfc'][:]
-    #replaces initial time step with 0's instead of NaNs
-    TP[0] = np.zeros((361,720))
-
-    u_wind = ds.variables['ugrd100m'][:]
-    v_wind = ds.variables['vgrd100m'][:]
-    #calculate the wind direction by U and V components
-    w100 = np.sqrt((u_wind*u_wind) + (v_wind*v_wind))
-
-    
-    return T2m,mslp,z500,w100,SSRD,TP,lat,lon 
-
-
-T2m,mslp,z500,w100,SSRD,TP,lat,lon = query_server('06z')
 
 
 
